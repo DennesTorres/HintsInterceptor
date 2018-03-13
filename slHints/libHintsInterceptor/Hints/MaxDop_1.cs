0bc19f08a6301c2988e3794999dc39d3e3aadf96
Sundeep Yama <SundeepYama@21stMortgage.com>
2018-03-13 16:23:31 -0400

    Updated HintBase to allow parameters for hints. Will convert underscore in hint class to space.

---
+++ slHints/libHintsInterceptor/Hints/MaxDop_1.cs
@@ -0,0 +1,17 @@
+﻿using libHintsInterceptor.HintKinds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libHintsInterceptor.Hints
+{
+    public class MaxDop_1 : QueryHint
+    {
+        public override bool CheckCompatibility(HintsCollection hints)
+        {
+            return true;
+        }
+    }
+}
